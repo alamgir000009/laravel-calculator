f3c04e857a699f2856762003b51f97cf14765406
alamgir-napollo <130369546+alamgir-napollo@users.noreply.github.com>
2024-03-14 03:27:36 +0500

    dd

---
+++ src/routes.php
@@ -0,0 +1,10 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('calculator',function(){
+    echo "Hello World";
+});
+
+Route::get('add/{a}/{b}',[\LP\Calculator\CalculatorController::class,'add']);
+Route::get('substract/{a}/{b}',[\LP\Calculator\CalculatorController::class,'substract']);
